9e887a7584f6116720f965b2853ae5d94ad13c9e
jpound <joe.pound21@gmail.com>
2014-07-21 10:41:16 -0400

    Adding scripts and first mocked elements.

--- BoldRadius.Kanban/BoldRadius.Kanban/Scripts/KanbanViewModel.js
+++ BoldRadius.Kanban/BoldRadius.Kanban/Scripts/KanbanViewModel.js
@@ -2,7 +2,68 @@
 
 var KanbanViewModel = {
 
+    board: {
+        projects: [
+                {
+                    name: "Project 1",
+                    statuses: [
+                        {
+                            name: "Ready",
+                            tasks: [
+                                {
+                                    description: "Project 1 Task 1"
+                                }
+                            ]
+                        },
+                        {
+                            name: "Doing",
+                            tasks: [
+                                {
+                                    description: "Project 1 Task 2"
+                                }
+                            ]
+                        },
+                        {
+                            name: "Done",
+                            tasks: [
+                                {
+                                    description: "Project 1 Task 3"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    name: "Project 2",
+                    statuses: [
+                        {
+                            name: "Ready",
+                            tasks: [
+                                {
+                                    description: "Project 2 Task 1"
+                                }
+                            ]
+                        },
+                        {
+                            name: "Doing",
+                            tasks: [
+                                {
+                                    description: "Project 2 Task 2"
+                                }
+                            ]
+                        },
+                        {
+                            name: "Done",
+                            tasks: [
+                                {
+                                    description: "Project 2 Task 3"
+                                }
+                            ]
+                        }
+                    ]
+                },
+            ]
+    }
 };
-
 
 ko.applyBindings(KanbanViewModel);
